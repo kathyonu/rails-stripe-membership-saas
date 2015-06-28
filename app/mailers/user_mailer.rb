cec7672a995320355e10d83f35f5fd1e6796f17a
Will Schive <lexigram@goodworksonearth.org>
2015-06-28 11:21:34 -0700

    clean up mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,16 @@ class UserMailer < ActionMailer::Base
   default from: "do-not-reply@example.com"
 
   def expire_email(user)
+    member = user.id
+    owner = User.first
+    mail(to: owner.email, subject: "Subscription Cancelled notice sent for Member ID \##{member}")
     mail(to: user.email, subject: "Subscription Cancelled")
   end
-=begin
 
-  # new to rails-stripe-membership-saas : requires webhooks to be in place, which are not yet in place
   def thanks_email(user)
+    member = user.id
+    owner = User.first
+    mail(to: owner.email, subject: "Thank You sent for green energy support from Stripe for Member ID \##{member}")
     mail(to: user.email, subject: "Thank You for your green energy support")
   end
 
@@ -18,7 +22,9 @@ class UserMailer < ActionMailer::Base
   end
 
   def plan_changed_email(user)
+    member = user.id
+    owner = User.first
+    mail(to: owner.email, subject: "A Members Membership Plan at Sequencers has been changed for Member ID \##{member}")
     mail(to: user.email, subject: "Your Membership Plan at Sequencers has been changed ..")
   end
-=end
 end
