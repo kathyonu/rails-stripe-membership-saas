7b9a4150129cd8b5321da102f0a1d32ad9078036
Will Schive <lexigram@goodworksonearth.org>
2016-03-04 17:02:35 -0800

    truenorth ruby-2.3.0 rails-4.2.5.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  # Settings specified here take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -26,6 +26,9 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
