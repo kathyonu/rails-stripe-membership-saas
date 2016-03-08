14140f96ad45a8bb49e71aa1db9f8ec6d71241c1
Will Schive <lexigram@goodworksonearth.org>
2016-03-08 08:50:12 -0800

    tests passing, cleaned up format

--- spec/stripe/stripe_subscription_spec.rb
+++ spec/stripe/stripe_subscription_spec.rb
@@ -1,10 +1,8 @@
 require 'stripe_mock'
-
 include Warden::Test::Helpers
 Warden.test_mode!
 
-describe 'Subscription API' do
-
+describe 'Subscription API, live: true' do
   let(:stripe_helper) { StripeMock.create_test_helper }
 
   before(:each) do
@@ -16,18 +14,24 @@ describe 'Subscription API' do
     Warden.test_reset!
   end
 
-  it "allows customer to cancel subscription" do
-    pending 'needs to be adjusted for current app which does not yet track the Stripe.customer id'
-    card_token = stripe_helper.generate_card_token(last4: "4242", exp_month: 12, exp_year: 2017)
+  it 'allows customer to cancel subscription' do
+    card_token = stripe_helper.generate_card_token(
+      last4: '4242',
+      exp_month: 12,
+      exp_year: 2017
+    )
     customer = Stripe::Customer.create(
-      email: 'cancel@example.com',
+      email: 'cancelsub@example.com',
       source: card_token,
-      description: "a customer cancellation",
+      description: 'a customer cancellation'
     )
     @user = FactoryGirl.create(:user, email: 'cancelsub@example.com')
     customer = Stripe::Customer.retrieve(customer.id)
-    @user.customer_id = customer.id
-    expect(@user.customer_id).to eq customer.id
+    expect(@user.email).to eq customer.email
+    expect(customer.sources.data[0].last4).to eq '4242'
+    expect(customer.sources.data[0].exp_month).to eq 12
+    expect(customer.sources.data[0].exp_year).to eq 2017
+
     # creating plan
     plan = stripe_helper.create_plan(id: 'my_plan', amount: 1500)
     # The above line replaces the following:
@@ -40,6 +44,7 @@ describe 'Subscription API' do
     # )
     expect(plan.id).to eq('my_plan')
     expect(plan.amount).to eq(1500)
+
     # add subscribing to subscription
     charge = Stripe::Charge.create({
       amount: 1500,
@@ -47,26 +52,27 @@ describe 'Subscription API' do
       interval: 'month',
       plan: 'silver',
       customer: customer.id,
-      description: 'a charge with a specific card',
+      description: 'a charge with a specific card'
       }, {
-        idempotency_key: "95ea4310438306ch"
+        idempotency_key: '95ea4310438306ch'
     })
     card_token = customer.sources.data[0].id
     charge = Stripe::Charge.retrieve(charge.id)
     customer = Stripe::Customer.retrieve(customer.id)
-    customer.subscriptions.create({plan: plan.id})
-    expect(card_token).to match /^test_cc/
-    expect(charge.id).to match /^test_ch/
-    expect(customer.id).to match /^test_cus/
-    expect(customer.sources.data[0].id).to match /^test_cc/
+    customer.subscriptions.create(plan: plan.id)
+    expect(card_token).to match(/^test_cc/)
+    expect(charge.id).to match(/^test_ch/)
+    expect(customer.id).to match(/^test_cus/)
+    expect(customer.sources.data[0].id).to match(/^test_cc/)
     expect(customer.sources.data.length).to eq 1
     expect(customer.sources.data[0].id).not_to be_nil
     expect(customer.sources.data[0].last4).to eq '4242'
+
     customer.subscriptions.create(plan: 'my_plan')
     customer.save
-    expect(customer.subscriptions[:url]).to match /\/v1\/customers\/test_cus\_.+\/subscriptions/
+    expect(customer.subscriptions[:url]).to match(%r{/v1/customers/test_cus_3/subscriptions})
     customer = Stripe::Customer.retrieve(customer.id)
-    expect(customer.subscriptions.data[0].id).to match /^test_su/
+    expect(customer.subscriptions.data[0].id).to match(/^test_su/)
     expect(customer.subscriptions.data[0].status).to eq 'active'
     subscription = customer.subscriptions.data[0]
     subscription.delete
@@ -76,51 +82,46 @@ describe 'Subscription API' do
   end
 
   it 'allows customer to delete their account' do
-    pending 'needs to be adjusted for current app which does not yet track the Stripe.customer id'
-    card_token = stripe_helper.generate_card_token(last4: "4242", exp_month: 12, exp_year: 2017)
+    card_token = stripe_helper.generate_card_token(
+      last4: '4242',
+      exp_month: 12,
+      exp_year: 2017
+    )
     customer = Stripe::Customer.create(
       email: 'cancelcus@example.com',
       source: card_token,
-      description: "a customer cancellation",
+      description: 'a customer cancellation'
     )
     @user = FactoryGirl.build(:user, email: 'cancelcus@example.com')
     customer = Stripe::Customer.retrieve(customer.id)
-    @user.customer_id = customer.id
-    expect(@user.customer_id).to eq customer.id
+    expect(@user.email).to eq customer.email
+
     # creating plan
     plan = stripe_helper.create_plan(id: 'my_plan', amount: 1500)
-    # The above line replaces the following:
-    # plan = Stripe::Plan.create(
-    #   :id => 'my_plan',
-    #   :name => 'StripeMock Default Plan ID',
-    #   :amount => 1500,
-    #   :currency => 'usd',
-    #   :interval => 'month'
-    # )
     expect(plan.id).to eq('my_plan')
     expect(plan.amount).to eq(1500)
-    # add subscribing to subscription
+
     charge = Stripe::Charge.create({
       amount: 1500,
       currency: 'usd',
       interval: 'month',
       customer: customer.id,
-      description: 'a charge with a specific card',
+      description: 'a charge with a specific card'
       }, {
-        idempotency_key: "95ea4310438306ch"
+        idempotency_key: '95ea4310438306ch'
     })
     card_token = customer.sources.data[0].id
     charge = Stripe::Charge.retrieve(charge.id)
     customer = Stripe::Customer.retrieve(customer.id)
-    expect(card_token).to match /^test_cc/
-    expect(charge.id).to match /^test_ch/
-    expect(customer.id).to match /^test_cus/
-    expect(customer.sources.data[0].id).to match /^test_cc/
+    expect(card_token).to match(/^test_cc/)
+    expect(charge.id).to match(/^test_ch/)
+    expect(customer.id).to match(/^test_cus/)
     expect(customer.sources.data.length).to eq 1
-    expect(customer.sources.data[0].id).not_to be_nil
+    expect(customer.sources.data[0].id).to match(/^test_cc/)
     expect(customer.sources.data[0].last4).to eq '4242'
+    expect(customer.sources.data[1]).to be nil
     customer.delete
-    expect(customer.id).to match /^test_cus/
+    expect(customer.id).to match(/^test_cus/)
     expect(customer.deleted).to be true
   end
 end
